a2b62da6d8ab5da85c8222061fd8daa87e355d7e
AndrewKraft <43654559+AndrewKraft@users.noreply.github.com>
2020-10-20 14:33:27 -0700

    finished tcp client and server, copied code to udp files as base

--- hw1/client_python_tcp.py
+++ hw1/client_python_tcp.py
@@ -1,21 +1,64 @@
 import socket
 import sys
+import time
 
-# lots of inspiration from https://docs.python.org/3/howto/sockets.html
-if len(sys.argv) > 1:
-	PORT = sys.argv[1]
-	if PORT < 1024 | PORT > 
-else:
-	PORT = 6969
-HOST = ''
-# code from https://docs.python.org/3/library/socket.html creates nonblocking tcp socket
-serversocket = socket.socket(
-	socket.AF_INET,
-	socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
-serversocket.bind((HOST, PORT))
-server.sock.listen(5)
+HOST = input('Enter server name or IP address: ')
+try:
+	PORT = int(input('Enter port: '), 10)
+except ValueError as e:
+	print('Invalid port number.')
+	exit(0)
+NUM_CLIENTS = 1
 
-while true:
-	(clientsocket, address) = serversocket.accept()
+if (PORT > 65353) | (PORT < 1024):
+	print('Invalid port number.')
+	exit(0)
 
-	
+clientsocks = []
+for i in range(NUM_CLIENTS):
+	clientsocks.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+for sock in clientsocks:
+	try:
+		sock.connect((HOST,PORT))
+		sock.setblocking(0)
+	except socket.error as e:
+		print('Could not connect to server.')
+		exit(0)
+
+cmd = {}
+for sock in clientsocks:
+	cmd[sock] = input('Enter command: ')
+	cmd[sock] = "%s%s" % (
+		'{:08X}'.format(len(cmd[sock])),
+		cmd[sock])
+
+for sock in clientsocks:
+	sock.settimeout(500)
+	try:
+		sock.send(cmd[sock].encode())
+	except socket.error as e:
+		print('Failed to send command. Terminating.')
+		del cmd[sock]
+		clientsocks.remove(sock)
+		sock.close()
+if clientsocks is None:
+	exit(0)
+
+for sock in clientsocks:
+	try:
+		inFromServer = sock.recv(8).decode()
+		inFromServer = sock.recv(int(inFromServer,16)).decode()
+		fname = 'client-' + time.strftime('%Y%m%d-%H%M%S') + '.txt'
+		with open(fname, 'w+') as f:
+			f.write(inFromServer)
+			print('File %s saved.' % fname)
+	except socket.error as e:
+		print('Did not recieve response.')
+		del cmd[sock]
+		clientsocks.remove(sock)
+		sock.close()
+if clientsocks is None:
+	exit(0)
+
+for sock in clientsocks:
+	sock.close()
---
+++ hw1/server_python_udp.py
@@ -0,0 +1,80 @@
+import select
+import socket
+import sys
+import subprocess
+import time
+
+# general inspiration from https://docs.python.org/3/howto/sockets.html, Author: Gordon McMillan
+PORT = 3300
+if len(sys.argv) > 1:
+	try:
+		PORT = int(sys.argv[1], 10)
+	except ValueError as e:
+		print('Invalid port number')
+		exit(0)
+	if (PORT < 1024) | (PORT > 65353):
+		print('Invalid port number.')
+		exit(0)
+HOST = ''
+
+serversocket = socket.socket(
+	socket.AF_INET,
+	socket.SOCK_DGRAM)
+serversocket.setblocking(0)
+
+serversocket.bind((HOST, PORT))
+
+serversocket.listen(5)
+print('socket listening on %s port %s' % (HOST, PORT))
+# specific usage of select from https://pymotw.com/2/select/, Author: Doug Hellmann
+
+inputs = [serversocket]
+outputs = []
+timeout = 500
+cmd = {} # socket to string
+
+
+while inputs:
+	print('waiting for next event')
+	readable, writeable, errors = select.select(inputs, outputs, inputs, timeout)
+	for sock in readable:
+		if sock is serversocket:
+			(clientsock, address) = sock.accept()
+			clientsock.setblocking(0)
+
+			print('connecting to client (\'%s\', %s)' % address)
+			inputs.append(clientsock)
+			cmd[clientsock] = ''
+		else:
+			data = sock.recv(8)
+			if data:
+				print('recieved %s bytes from %s' % (data.decode(),sock.getpeername()))
+				cmd[sock] = sock.recv(int(data.decode(),16)).decode()
+				if sock not in outputs:
+					outputs.append(sock)
+			else:
+				if sock in outputs:
+					outputs.remove(sock)
+				inputs.remove(sock)
+				sock.close()
+	for sock in writeable:
+		result = subprocess.run(cmd[sock], shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		
+		fname = 'server-' + time.strftime('%Y%m%d-%H%M%S') + '.txt'
+		outToClient = ''
+		with open(fname, 'w+') as f:
+			f.write(result.stdout)
+		with open(fname) as f:
+			outToClient = f.read()
+
+		outToClient = '%s%s' % ('{:08X}'.format(len(outToClient)), outToClient)
+		sock.send(outToClient.encode())
+		outputs.remove(sock)
+		del cmd[sock]
+	for sock in errors:
+		if sock in outputs:
+			outputs.remove(sock)
+		inputs.remove(sock)
+		del cmd[sock]
+		sock.close()
+			
